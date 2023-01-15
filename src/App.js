a2c755f9160fb04a439b5c7bcf88c4b4f176d5af
Marika Mäki <zeracheciel@gmail.com>
2023-01-15 23:09:39 +0200

    Tehtava 5

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Expenses from './components/Expenses';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
@@ -27,28 +28,10 @@ function App() {
 
   return (
     <div>
-      <ExpenseItem
-            title={expenses[0].title}
-            amount={expenses[0].amount}
-            date={expenses[0].date}
-      />
-      <ExpenseItem
-            title={expenses[1].title}
-            amount={expenses[1].amount}
-            date={expenses[1].date}
-      />
-      <ExpenseItem
-            title={expenses[2].title}
-            amount={expenses[2].amount}
-            date={expenses[2].date}
-      />
-      <ExpenseItem
-            title={expenses[3].title}
-            amount={expenses[3].amount}
-            date={expenses[3].date}
-      />
+      <Expenses items={expenses}/>
       <p>Marika Mäki / VAMK / BIT</p>
       </div>
+      
   );
 }
 
