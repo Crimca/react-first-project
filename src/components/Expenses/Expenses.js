4b56a814fc78802d1d6a222c470d199a24857bde
Mäki <zeracheciel@gmail.com>
2023-01-24 14:21:07 +0200

    Ratkaisu 1 tehtävään 15

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,7 +6,7 @@ import './Expenses.css';
 
 const Expenses = (props) => {
 
-const [filteredYear, setFilteredYear] = useState('2024');
+const [filteredYear, setFilteredYear] = useState('2023');
 
       const filterChangeHandler = (selectedYear) => {
             //console.log('Expenses.js');
@@ -14,18 +14,26 @@ const [filteredYear, setFilteredYear] = useState('2024');
             setFilteredYear(selectedYear);
       };
 
+      const filteredExpenses = props.items.filter(expense => {
+       return expense.date.getFullYear().toString() === filteredYear;
+      });
+
       return (
         <Card className="expenses">
-          <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {props.items.map((expense) => (
+          <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}
+          />
+      {filteredExpenses.map((expense) => (
+            //{props.items.map((expense) => (
       <ExpenseItem
             key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
-      />
+            />
       ))}
-</Card>
+      </Card>
     );
 }
 
